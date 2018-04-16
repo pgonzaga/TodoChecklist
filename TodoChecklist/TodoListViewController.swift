3b784777bbdfed350d307075ca7ea72b161c82e4
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2018-04-16 00:06:16 -0300

    Adds todo list first version

---
+++ TodoChecklist/TodoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TodoChecklist
+//
+//  Created by Pablo Gonzaga on 4/15/18.
+//  Copyright © 2018 Pablo Gonzaga. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var items: [String] = ["test1", "test2"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK TableView Data source
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+}
+
